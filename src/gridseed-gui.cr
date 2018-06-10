919d7199f0023e5ae70890421c5d49e2f3fe7b8e
fukaoi <goodakira7@gmail.com>
2018-06-10 22:12:18 +0900

    Changed css engine

--- src/gridseed-gui.cr
+++ src/gridseed-gui.cr
@@ -1,4 +1,5 @@
 require "kemal"
+require "kemal-watcher"
 require "./gridseed-gui/*"
 
 # TODO: Write documentation for `Gridseed::Gui`
@@ -7,7 +8,13 @@ module Gridseed::Gui
 end
 
 get "/" do
-  render "view/index.ecr"
+  # render "view/index.ecr"
 end
 
+files = [
+  "public/*.js",
+  "public/*.html"
+]
+
+Kemal.watch(files)
 Kemal.run
